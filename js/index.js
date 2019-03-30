ad9d22b0b1436b474c955fb30a9c94193265e564
RenanSucs <47055395+RenanSucs@users.noreply.github.com>
2019-03-30 19:47:06 -0300

    Api consultada

--- js/index.js
+++ js/index.js
@@ -1,35 +1,27 @@
 var flickr = {
-    render:function(){
-      var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=c6d969c8010a6ad38eef8fbb05107098&text=business&format=json&nojsoncallback=1&api_sig=7abb1810a840c9614cb21fefea26276b";
-      var galeriaItem = document.querySelector('.galeria-item');
-      $.ajax({
-        url: url,
-        type: "GET",
-       success: function(data){
-        var string = "";
-        
-        console.log(data.photos.id);
-          
-    
-       },
-       error: function(){
-        console.log("So sorry!");
-       }
-      });
-    }
-  };
-  flickr.render();
+  render:function(){
+    var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=c6d969c8010a6ad38eef8fbb05107098&text=business&format=json&nojsoncallback=1&api_sig=7abb1810a840c9614cb21fefea26276b";
+    var galeriaItem = document.querySelector('.galeria-item');
+    $.ajax({
+      url: url,
+      type: "GET",
+     success: function(data){
+      var string = "";
 
-  for(var i = 0; i<data.data.results.length;i++){
-    var element = data.data.results[i];
-    string += "<a href='"+element.urls[0].url+"'target='_blank'>";
-    string += "<div class='card-thumb-caracters'>";
-    string += "<figure class='img-wrapper'>";
-    string += "<img src='"+element.thumbnail.path+"/portrait_uncanny."+element.thumbnail.extension+"'/>";
-    string += "</figure>";
-    string += "</div>";
-    string += "<div class='is-sliding-caracters'>";
-    string += "<p class='card-headline'>"+element.name+"</p>";
-    string += "</div>";
+      for(var i = 0; i<6;i++){
+      var album = data.photos.photo[i];
+      var urlPhotos = 'https://farm'+album.farm+'.staticflickr.com/'+album.server+'/'+album.id+'_'+album.secret+'.jpg';
+      
+      string += "<img src='"+urlPhotos+"'/>";
+      }
+      
+      galeriaItem.innerHTML = string;
+     },
+     error: function(){
+      console.log("So sorry!");
+     }
+    });
   }
-  marvelCharacter.innerHTML = string;
+};
+flickr.render();
+ 
